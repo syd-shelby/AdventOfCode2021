7d18e1bb8c0404908025ecd97104de99b6a4608a
syd-shelby <sshelby3@vols.utk.edu>
2021-12-07 09:57:43 -0600

    solution 2

---
+++ day7/crabs.py
@@ -0,0 +1,25 @@
+#get an array of crabs read in
+f = open('input.txt', 'r')
+line = f.readline()
+nums = map(int,line.split(','))
+f.close()
+
+#find min and max
+max = max(nums)
+min = min(nums)
+
+fuellist = [0] * (max-min+1)
+minfuel = -1
+pos = 0
+
+for x in range(min, max+1):
+	fuel = 0
+	for n in nums:
+		steps = abs(n-x)
+		fuel += steps*(steps+1)/2
+	if minfuel == -1 or fuel < minfuel:
+		minfuel = fuel
+		pos = x
+
+
+print minfuel, "fuel required for position: ", pos
